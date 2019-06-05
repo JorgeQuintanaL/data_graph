96647627daf9bb132278640e24b145e4afeb90ff
JorgeQuintana <jorge.quintana.l@icloud.com>
2019-06-05 17:04:12 -0500

    SOme changes and some things to fix

--- dg/lines.py
+++ dg/lines.py
@@ -9,16 +9,20 @@ class Lines():
     def __init__(self):
         self._data = []
 
-    def plot(self, x: list, y: list, title: str, xlabel: str,
-             ylabel: str, labs: str, plot: bool):
-        self._data = [x, y]
-        for i in range(len(x)):
-            plt.plot(x[i], y[i])
-            plt.xlabel(xlabel)
-            plt.ylabel(ylabel)
-        plt.legend(labs)
-        if plot:
-            plt.show()
+    def plot(self, x: list, y: list, plot: bool, title="Some Title",
+             xlabel=["Some Label"], ylabel="Some Label", labs="Some Label"):
+        if len(x) == len(y):
+            self._data = [x, y]
+            for i in range(len(x)):
+                plt.plot(x[i], y[i])
+                plt.xlabel(xlabel)
+                plt.ylabel(ylabel)
+            plt.legend(labs)
+            if plot:
+                plt.show()
+        else:
+            return "Series sizes should match!: \
+                    x is {} and y is {}".format(len(x), len(y))
 
     def __len__(self):
         return len(self._data)
--- tests/test_lines.py
+++ tests/test_lines.py
@@ -8,39 +8,33 @@ import os
 
 
 @pytest.fixture
+def generate_x():
+    return np.arange(1, 100, 1)
+
+
+@pytest.fixture
+def generate_y(generate_x):
+    return np.power(generate_x, 2)
+
+
+@pytest.fixture
 def lines():
     return Lines()
 
 
 @pytest.fixture(params=[0, 1])
-def plot_lines(lines, request):
+def plot_lines(generate_x, generate_y, lines, request):
     if request.param == 0:
         x = []
         y = []
-        title = ""
-        x_label = ""
-        y_label = ""
-        labels = ""
-        lines.plot(x=x,
-                   y=y,
-                   title=title,
-                   xlabel=x_label,
-                   ylabel=y_label,
-                   labs=labels,
-                   plot=True)
-    else:
-        x = np.arange(1, 100, 1)
-        y = np.power(x, 2)
-        title = "Some Title"
-        x_label = "Some Values"
-        y_label = "Some Values"
-        labels = ["Some Label"]
         lines.plot(x=[x],
                    y=[y],
-                   title=title,
-                   xlabel=x_label,
-                   ylabel=y_label,
-                   labs=labels,
+                   plot=True)
+    else:
+        x = generate_x
+        y = generate_y
+        lines.plot(x=[x],
+                   y=[y],
                    plot=True)
     return lines
 
@@ -70,11 +64,15 @@ def test_constructor(lines):
 
 
 def test_plot(plot_lines):
+    assert isinstance(plot_lines, Lines)
     assert len(plot_lines) == 2
+    assert plot_lines.shape("x") == plot_lines.shape("y")
 
 
 def test_multiple_plots(multiple_lines):
+    assert isinstance(multiple_lines, Lines)
     assert len(multiple_lines) == 2
+    assert multiple_lines.shape("x") == multiple_lines.shape("y")
 
 
 def test_save_images(plot_lines):
