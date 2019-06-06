8bce5aa81e0196161c696967a75181f79a38f9de
JorgeQuintana <jorge.quintana.l@iclooud.com>
2019-06-06 10:45:17 -0500

    Refactoring tests usingg fixtures

--- tests/test_lines.py
+++ tests/test_lines.py
@@ -40,20 +40,14 @@ def plot_lines(generate_x, generate_y, lines, request):
 
 
 @pytest.fixture
-def multiple_lines(lines):
-    x1 = np.arange(1, 100, 1)
-    y1 = np.power(x1, 2)
-    x2 = np.arange(1, 100, 1)
-    y2 = np.power((x2 - 2), 2)
-    title = "Some Title"
-    x_label = "Some Values"
-    y_label = "Some Values"
+def multiple_lines(generate_x, generate_y, lines):
+    x1 = generate_x
+    y1 = generate_y
+    x2 = generate_x
+    y2 = generate_y + 20
     labels = ["Some Label 1", "Some Label 2"]
     lines.plot(x=[x1, x2],
                y=[y1, y2],
-               title=title,
-               xlabel=x_label,
-               ylabel=y_label,
                labs=labels,
                plot=True)
     return lines
@@ -66,13 +60,11 @@ def test_constructor(lines):
 def test_plot(plot_lines):
     assert isinstance(plot_lines, Lines)
     assert len(plot_lines) == 2
-#    assert plot_lines.shape("x") == plot_lines.shape("y")
 
 
 def test_multiple_plots(multiple_lines):
     assert isinstance(multiple_lines, Lines)
     assert len(multiple_lines) == 2
-#    assert multiple_lines.shape("x") == multiple_lines.shape("y")
 
 
 def test_save_images(plot_lines):
