f235fc6cb8aa61e11f36e09867d1323fe278866d
JorgeQuintana <jorge.quintana.l@icloud.com>
2019-06-07 12:18:45 -0500

    Refactor using a Plot class as parent to share the properties with child plots

--- dg/lines.py
+++ dg/lines.py
@@ -5,12 +5,30 @@ Python module to create lines plots using matplotlib as backend
 import matplotlib.pyplot as plt
 
 
-class Lines():
+class Plot():
     def __init__(self):
         self.x = None
         self.y = None
         self.f = None
         self.axs = None
+        self.layout = None
+
+    def apply_layout(self):
+        self.layout = [{"background_color": "#c5eff7",
+                        "grid_color": '#aea8d3'}]
+        self.f.patch.set_facecolor("#c5eff7")
+        self.axs.set_facecolor('#aea8d3')
+
+    def save(self, name):
+        self.f.savefig(name)
+
+    def show(self):
+        self.f.show()
+
+
+class Lines(Plot):
+    def __init__(self):
+        Plot.__init__(self)
 
     def plot(self, x: list, y: list, title="Some Title",
              xlabel="Some Label", ylabel="Some Label", labs="Some Label"):
@@ -24,15 +42,3 @@ class Lines():
             self.axs.set_xlabel(xlabel)
             self.axs.set_ylabel(ylabel)
             self.axs.legend(labs)
-
-    def apply_layout(self):
-        self.layout = [{"background_color": "#c5eff7",
-                        "grid_color": '#aea8d3'}]
-        self.f.patch.set_facecolor("#c5eff7")
-        self.axs.set_facecolor('#aea8d3')
-
-    def save(self, name):
-        self.f.savefig(name)
-
-    def show(self):
-        self.f.show()
